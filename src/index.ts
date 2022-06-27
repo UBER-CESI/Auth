6ae30327802d30ca28ad715e1b2164c8f092f102
TienYou-Nathan <nathan.tienyou@viacesi.fr>
2022-06-27 22:49:50 +0200

    fix Cors Again

--- src/index.ts
+++ src/index.ts
@@ -105,6 +105,7 @@ app.use(`/`, (req, res, next) => {
     res.set({ 'Access-Control-Allow-Origin': "http://localhost:8100" })
     res.set({ 'Access-Control-Allow-Methods': "POST, OPTIONS, GET, PUT, DELETE" })
     res.set({ 'Access-Control-Allow-Headers': "Content-Type" })
+    res.set({ 'Access-Control-Allow-Credentials': true })
   }
   next()
 })
